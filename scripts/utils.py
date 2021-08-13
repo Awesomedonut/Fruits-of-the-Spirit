4168c4c6053e5d1bd10d9614c3e7e2e602ed5479
Awesomedonut <jasperyjsong@gmail.com>
2021-08-12 20:45:12 -0700

    Loaded audio, added image draw function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,42 @@
 import os
 import pygame
 
+
 def load_all_images():
     graphics = {}
     dirs = os.listdir(os.path.join("resources", "graphics"))
     for file in dirs:
         file_name, _ = os.path.splitext(file)
         print(os.path.splitext(file))
-        print(file_name)
+       # print(file_name)
         file_image = pygame.image.load(os.path.join("resources", "graphics", file))
         graphics[file_name] = file_image
         
     return graphics
+
+def draw(win):
+    image = load_all_images()["mario_bros"]
+    win.fill((200, 12, 12))
+    win.blit(image, (0, 0))
+
+def load_all_music():
+    music = {}
+    dirs = os.listdir(os.path.join("resources", "music"))
+    for file in dirs:
+        file_name, _ = os.path.splitext(file)
+        print(os.path.splitext(file))
+        print(file_name)
+        music[file_name] = os.path.join("resources", "music", file)
+    return music
     
+def load_all_sound_effects():
+    sounds = {}
+    dirs = os.listdir(os.path.join("resources", "sound"))
+    for file in dirs:
+        file_name, _ = os.path.splitext(file)
+        print(os.path.splitext(file))
+        print(file_name)
+        sounds[file_name] = os.path.join("resources", "sound", file)
+    return sounds
 
     
--- start.py
+++ start.py
@@ -1,13 +1,27 @@
 import pygame
 from scripts.main import main
-from scripts.utils import load_all_images
-    
-if __name__ == "__main__":
+from scripts.utils import load_all_images, load_all_music, load_all_sound_effects, draw
 
-    main()
+if __name__ == "__main__":
+    pygame.init()
+   # main()
+   
+    # for key, value in load_all_images().items():
+    #     print(key, value)
+    # for key, value in load_all_music().items():
+    #     print(key, value)
+    # for key, value in load_all_sound_effects().items():
+    #     print(key, value)
+    win = pygame.display.set_mode((500, 300))
+ 
+    while True: #always running
+        pygame.time.Clock().tick(60) 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            else:
+                draw(win)
+        
+                pygame.display.update()
+
     
-    
-    for key,value in load_all_images().items():
-        print(key, value)
-    
-    pygame.quit()
